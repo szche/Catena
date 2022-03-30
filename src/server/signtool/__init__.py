5729c893db4cb9e3de532069fbfc5c6d2a2ea3ee
szche <github@chadam.pl>
2022-03-30 07:50:46 -0700

    Fix signature verification bug

--- src/server/signtool/__init__.py
+++ src/server/signtool/__init__.py
@@ -1,34 +1,29 @@
 import subprocess
 
 class SignTool():
-		def __init__(self):
-				pass
-
 		def _run_command(self, cmd):
 				c = subprocess.run(cmd, capture_output=True, shell=True)
 				# Return code 0 is success 
-				# TODO files without signatures also return 0
-				if c.returncode == 0:
-					print(c.stdout)
+				if c.returncode == 0 and "No signature found" not in c.stdout.decode():
+					#print(c.stdout.decode())
 					return True
-				print('-'* 20)
-				print("ERROR")
-				print(c.stderr.decode())
-				print('-'* 20)
+				#print('-'* 20)
+				#print("ERROR")
+				#print(c.stderr.decode())
+				#print('-'* 20)
 				return False
-
 
 		def sign(self, certfile, keyfile, description, url, filepath, filepath_signed):
 				# Needs to be executed with sudo to be working correctly
 				command = f'osslsigncode sign -h sha2 -certs {certfile} ' + \
 							f'-key {keyfile} -n {description} -i {url} ' + \
 							f'-in {filepath} -out {filepath_signed}'
-				print('Final command: ', command)
-				print( self._run_command(command) )
+				#print('Final command: ', command)
+				return self._run_command(command)
 
 		def verify(self, filepath):
 				command = f'osslsigncode verify {filepath}'
-				print( self._run_command(command) )
+				return self._run_command(command)
 
 
 if __name__ == "__main__":
