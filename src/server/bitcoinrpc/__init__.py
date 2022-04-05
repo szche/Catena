464f66a5db7bbf617ae7b7a89f314842bcbae5af
szche <github@chadam.pl>
2022-04-05 08:28:38 -0700

    Bitcoinlib catena

---
+++ src/server/bitcoinrpc/__init__.py
@@ -0,0 +1,80 @@
+from bitcoinlib.wallets import Wallet 
+from bitcoinlib.wallets import wallets_list
+from bitcoinlib.transactions import Output
+from bitcoinlib.keys import Address
+#from bitcoinlib.values import Value
+
+NETWORK = 'testnet'
+WALLET_NAME = 'Catena'
+
+class Bitcoin():
+
+		def __init__(self, network):
+				# Check if wallet named 'Catena' alerady exsits
+				# Connects to it if so, creates it otherwise
+				if self._check_if_exists(WALLET_NAME):
+					self.wallet = self._connect_wallet(WALLET_NAME)
+					self.wallet.balance_update_from_serviceprovider()
+				else:
+					self.wallet = self._create_wallet(WALLET_NAME, network)
+				#for key, value in self.wallet.as_dict().items():
+				#			print(key, value)
+
+		
+		def _check_if_exists(self, name):
+				wallets = wallets_list()
+				for w in wallets:
+						if w['name'] == name:
+								return True
+				return False
+		
+		def _create_wallet(self, name, network):
+			return Wallet.create(name, network=network)
+
+		def _connect_wallet(self, name):
+			return Wallet(name)
+
+		@property
+		def balance(self):
+			self.wallet.balance_update_from_serviceprovider()
+			return self.wallet.balance()
+
+		@property
+		def address(self):
+			print(self.wallet.addresslist())
+			print(Address(self.wallet.addresslist()[0]))
+			return self.wallet.addresslist()[0]
+	
+
+		def new_log(self, hashed_data):
+			self.wallet.utxos_update()
+			len_data = len(hashed_data)
+			len_data_hex = hex(len_data//2)[2:]
+			lock_script = f'6a{len_data_hex}{hashed_data}'
+			address = self.address
+			# TODO smarter fee estimation
+			# Currently fee = 700 sats
+			fee = 700
+			catena_output = Output(0, script_type='nulldata', lock_script=lock_script)
+			# Something wrong with the addesss here
+			change_output = Output(value=self.balance-fee, address=Address(address))
+			#print(catena_output)
+			#print(change_output)
+			outputs = [change_output, catena_output]
+			inputs = self.wallet.select_inputs(self.balance)
+			tx = self.wallet.transaction_create(output_arr=outputs, \
+							input_arr=inputs, network=NETWORK)
+			tx.sign()
+			# For debugging dont send it yet
+			#tx.send()
+			print(tx.info())
+
+
+
+
+if __name__ == "__main__":
+	btc = Bitcoin(NETWORK)
+	print(btc.balance)
+	print(btc.address)
+	#btc.new_log('9afbd907e01dae46f785a6db90cf1f9090158312f58b7cd67593c224ce17fc74')
+
